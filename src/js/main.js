98594bac506e00b3d8a2832b965dea2f4eb3cadb
Alexei1974 <lelya.gaysin@mail.ru>
2022-02-15 12:26:40 +0300

    added slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
 $(function(){
 
+    $('.carausel__inner').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 900,
+        // adaptiveHeight: true,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        // fade: true,
+        // cssEase: 'linear',
+        prevArrow: '<button type="button" class="carausel-arrow slick-prev-carausel"><img src="images/slider/chevron_right.png" alt=""></button>',
+        nextArrow: '<button type="button" class="carausel-arrow slick-next-carausel"><img src="images/slider/chevron_left.png" alt=""></img></button>',
+        responsive: [
+            {
+                breakpoint: 993,
+                settings: {
+                   arrows:false,
+                   dots:true
+                }
+            },
+        ]
+    });
 });
