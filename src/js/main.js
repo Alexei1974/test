edd628f6a1cb8e380492488bd6e3d77f3b9012c2
Alexei1974 <lelya.gaysin@mail.ru>
2022-02-19 16:07:20 +0300

    form validate

--- src/js/main.js
+++ src/js/main.js
@@ -41,4 +41,62 @@ $(function () {
     tooleSlide('.catalog-item__link');
     tooleSlide('.catalog-item__bask');
 
+    //============== modal=========================
+    $('[data-modal=consultion]').on('click', function () {
+        $('.overlay, #consultion').fadeIn('slow');
+        $('body').addClass('no-scroll')
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay,#consultion, #order, #tranks').fadeOut('slow');
+        $('body').removeClass('no-scroll')
+    });
+
+    $('.catalog-item__btn').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__subtitle').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+            $('body').addClass('no-scroll');
+        });
+    });
+
+    // ========= form validate==========
+    function validForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2,
+                },
+                phone: 'required',
+                email: {
+                    required: true,
+                    email: true,
+                },
+            },
+            messages: {
+                name: {
+                    required: "Укажите ваше имя",
+                    minlength: jQuery.validator.format(
+                        "Ваше  имя  должно  быть  не  менее  2х символов"
+                    ),
+                },
+                email: {
+                    required:
+                        "Нам нужен ваш адрес электронной почты, чтобы с вами связаться",
+                    email:
+                        "Ваш адрес электронной почты должен быть в формате name@domain.com",
+                },
+                phone: {
+                    required: "Введите ваш номер телефона.",
+
+                },
+            },
+        });
+    };
+
+    validForms('#consultation-form');
+    validForms('#consultion form');
+    validForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 });
